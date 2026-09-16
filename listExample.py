38647b1d0063dd3504cdbd44990244107901a96a
unknown <amit.adenova@gmail.com>
2026-09-16 18:21:07 +0100

    list concept example

---
+++ listExample.py
@@ -0,0 +1,7 @@
+# List Creation
+
+mylist = ["Hello",232,293.0,3+4j,'Application',66,88,44,44,44,66,88]
+print("List value ",mylist)
+# concept is :
+# 1. List any type of homogenious datatype can store, order will be preserved, duplication will allowed,
+# Nature in growable.
